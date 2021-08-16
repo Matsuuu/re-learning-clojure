fe08c4a63966f25201a37874c3f956e7bad50e1b
Matsuuu <huhta.matias@gmail.com>
2021-08-16 19:53:34 +0300

    Chapter 3

--- hobbit/src/hobbit/hobbit.clj
+++ hobbit/src/hobbit/hobbit.clj
@@ -58,3 +58,16 @@
        (recur (f result (first remaining)) (rest remaining)))))
   ([f [head & tail]]
    (my-reduce f head tail)))
+
+(defn hit
+  [asym-body-parts]
+  (let [sym-parts (better-symmetrize-body-parts asym-body-parts)
+        body-part-size-sum (reduce + (map :size sym-parts))
+        target (rand body-part-size-sum)]
+    (loop [[part & remaining] sym-parts
+           accumulated-size (:size part)]
+      (if (> accumulated-size target)
+        part
+        (recur remaining (+ accumulated-size (:size (first remaining))))))))
+
+(hit asym-hobbit-body-parts) ; {:name "left-thigh", :size 4}
