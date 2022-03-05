dd23d073e49884c7df90eb490e8eb76a8426b101
Matsuuu <huhta.matias@gmail.com>
2022-03-05 19:18:01 +0200

    Core functions done

--- core-functions/src/core_functions/core.clj
+++ core-functions/src/core_functions/core.clj
@@ -1,5 +1,7 @@
 (ns core-functions.core
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [clojure.string :as string]))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -99,3 +101,89 @@
 (sort-by count ["aaa" "c" "bb"]) ; ("c" "bb" "aaa")
 
 (concat [1 2] [3 4]) ; (1 2 3 4)
+
+(def vampire-database
+  {0 {:makes-blood-puns? false, :has-pulse? true  :name "McFishwich"}
+   1 {:makes-blood-puns? false, :has-pulse? true  :name "McMackson"}
+   2 {:makes-blood-puns? true,  :has-pulse? false :name "Damon Salvatore"}
+   3 {:makes-blood-puns? true,  :has-pulse? true  :name "Mickey Mouse"}})
+
+(defn vampire-related-details
+  [social-security-number]
+  (Thread/sleep 1000)
+  (get vampire-database social-security-number))
+
+(defn vampire?
+  [record]
+  (and (:makes-blood-puns? record)
+       (not (:has-pulse? record))
+       record))
+
+(defn identify-vampire
+  [social-security-numbers]
+  (first (filter vampire?
+                 (map vampire-related-details social-security-numbers))))
+
+(time (vampire-related-details 0)) ; {:makes-blood-puns? false, :has-pulse? true, :name "McFishwich"}
+
+(def mapped-details (map vampire-related-details (range 0 10000000)))
+
+;(time (identify-vampire (range 0 100000)))
+
+(concat (take 8 (repeat "na")) ["Batman!"])
+
+(take 3 (repeatedly (fn [] (rand-int 10))))
+
+(defn even-numbers
+  ([] (even-numbers 0))
+  ([n] (cons n (lazy-seq (even-numbers (+ n 2))))))
+
+(take 20 (even-numbers)) ; (0 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38)
+
+(map identity {:sunlight-reaction "Glitter!"})
+
+(into {} (map identity {:sunlight-reaction "Glitter!"}))
+
+(into {:favorite-emotion "gloomy"} [[:sunlight-reaction "Glitter!"]]) ; {:favorite-emotion "gloomy", :sunlight-reaction "Glitter!"}
+
+(into ["cherry"] '("pine" "spruce")) ; ["cherry" "pine" "spruce"]
+
+(conj [0] [1]) ; [0 [1]]
+(into [0] [1]) ; [0 1]
+
+(conj [0] 1) ; [0 1]
+
+(max 1 2 3) ; 3
+
+(apply max [0 1 2]) ; 2
+
+(def add10 (partial + 10))
+
+(add10 3) ; 13
+(add10 5) ; 15
+
+(def add-missing-elements
+  (partial conj ["water" "earth" "air"]))
+
+(add-missing-elements "unobtainium" "adamantium")
+
+(defn lousy-logger
+  [log-level message]
+  (condp = log-level
+    :warn (string/lower-case message)
+    :emergency (string/upper-case message)))
+
+(def warn (partial lousy-logger :warn))
+(def emergency (partial lousy-logger :emergency))
+
+(warn "Red light ahead") ; "red light ahead"
+(emergency "Red light ahead") ; "RED LIGHT AHEAD"
+
+(def not-vampire? (complement vampire?))
+
+(defn identify-humans
+  [social-security-numbers]
+  (filter not-vampire?
+          (map vampire-related-details social-security-numbers)))
+
+
