4ac9b38dd51286950113f657981df799e4ddef10
Matsuuu <huhta.matias@gmail.com>
2022-03-05 17:48:39 +0200

    Let's start getting lazy

--- core-functions/src/core_functions/core.clj
+++ core-functions/src/core_functions/core.clj
@@ -32,4 +32,70 @@
 
 (stats [3 4 10]) ; (17 3 17/3)
 
-(stats [80 1 44 13 6])
+(stats [80 1 44 13 6]) ; (144 5 144/5)
+
+(reduce (fn [new-map [key val]]
+          (assoc new-map key (inc val)))
+        {}
+        {:max 30 :min 10}) ; {:max 31, :min 11}
+
+(assoc (assoc {} :max (inc 30))
+       :min (inc 10)) ; {:max 31, :min 11}
+
+(reduce (fn [new-map [key val]]
+          (if (> val 4)
+            (assoc new-map key val)
+            new-map))
+        {}
+        {:human 4.1
+         :critter 3.9}) ; {:human 4.1}
+
+(take 3 [1 2 3 4 5 6 7 8 9 10]) ; (1 2 3)
+(drop 3 [1 2 3 4 5 6 7 8 9 10])
+; (4 5 6 7 8 9 10)
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(take-while #(< (:month %) 3) food-journal)
+; ({:month 1, :day 1, :human 5.3, :critter 2.3}
+;  {:month 1, :day 2, :human 5.1, :critter 2.0}
+;  {:month 2, :day 1, :human 4.9, :critter 2.1}
+;  {:month 2, :day 2, :human 5.0, :critter 2.5})
+
+(drop-while #(< (:month %) 3) food-journal)
+; ({:month 3, :day 1, :human 4.2, :critter 3.3}
+;  {:month 3, :day 2, :human 4.0, :critter 3.8}
+;  {:month 4, :day 1, :human 3.7, :critter 3.9}
+;  {:month 4, :day 2, :human 3.7, :critter 3.6})
+
+(take-while #(< (:month %) 4)
+            (drop-while #(< (:month %) 2) food-journal))
+; ({:month 2, :day 1, :human 4.9, :critter 2.1}
+;  {:month 2, :day 2, :human 5.0, :critter 2.5}
+;  {:month 3, :day 1, :human 4.2, :critter 3.3}
+;  {:month 3, :day 2, :human 4.0, :critter 3.8})
+
+(filter #(< (:human %) 5) food-journal)
+; ({:month 2, :day 1, :human 4.9, :critter 2.1}
+;  {:month 3, :day 1, :human 4.2, :critter 3.3}
+;  {:month 3, :day 2, :human 4.0, :critter 3.8}
+;  {:month 4, :day 1, :human 3.7, :critter 3.9}
+;  {:month 4, :day 2, :human 3.7, :critter 3.6})
+
+(some #(> (:critter %) 5) food-journal) ; nil
+
+(some #(> (:critter %) 3) food-journal) ; true
+
+(sort [3 1 2]) ; (1 2 3)
+
+(sort-by count ["aaa" "c" "bb"]) ; ("c" "bb" "aaa")
+
+(concat [1 2] [3 4]) ; (1 2 3 4)
